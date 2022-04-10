facb4736f098852526090f7351ee56391d30b172
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 15:53:34 -0500

    added boilerplate for a message syncing function

---
+++ utils/sync.js
@@ -0,0 +1,9 @@
+const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
+const {MessageEmbed, CommandInteractionOptionResolver, Message} = require('discord.js');
+
+
+const syncMessage = () => {
+
+}
+
+export default syncMessage
