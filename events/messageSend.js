3669f8bc4abf4203dc02c018983dbf470f07ca37
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 17:30:09 -0500

    it now executes the code logic

--- events/messageSend.js
+++ events/messageSend.js
@@ -1,8 +1,11 @@
+const { default: syncMessage } = require("../utils/sync");
+
 //This logs all interactions
 module.exports = {
 	name: 'messageCreate',
 	execute(msg) {
 		console.log(`${msg.author} sent a message at ${msg.createdAt} `);
+		syncMessage(msg)
 	},
 };
  
