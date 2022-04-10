2f10ce87beef27a0663d1a54cd02e5343cc90b13
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 17:12:06 -0500

    added parameter for the function and an import to the config

--- utils/sync.js
+++ utils/sync.js
@@ -1,8 +1,9 @@
 const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
 const {MessageEmbed, CommandInteractionOptionResolver, Message} = require('discord.js');
+const { serverOne, serverTwo } = require('../config.json') 
 
 
-const syncMessage = () => {
+const syncMessage = (msg) => {
 
 }
 
