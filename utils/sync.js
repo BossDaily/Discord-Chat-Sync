9ef1298d2ca505f0c6fe9a800aa55616720e3b1b
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 17:58:50 -0500

    added extra logic to prevent crashes

--- utils/sync.js
+++ utils/sync.js
@@ -4,28 +4,34 @@ const { serverOne, serverTwo } = require('../config.json')
 
 
 const syncMessage = (msg) => {
-    const userPfp = msg.member.displayAvatarURL( {dynamic: true})
-    const userName = msg.member.username
+    const userPfp = msg.member.displayAvatarURL({format: 'png'})
+    const userName = msg.member.displayName
     const userContent = msg.content
     const channelid = msg.channelId.toString()
 
     const webhookOne = new WebhookClient({ url: `${serverOne.webhook}` });
     const webhookTwo = new WebhookClient({ url: `${serverTwo.webhook}`});
 
-    if(channelid === serverOne.channel){
+    if(userPfp === null) {
+        console.log('this message will not sync')
+    } else if(channelid === serverOne.channel){
         webhookTwo.send({
             content: `${userContent}`,
             username: `${userName}`,
             avatarURL: `${userPfp}`
         })
+
+        console.log(userPfp)
     } else if (channelid === serverTwo.channel){
         webhookOne.send({
             content: `${userContent}`,
             username: `${userName}`,
             avatarURL: `${userPfp}`
         })
+        console.log(userPfp)
     } else {
         console.log('this message will not sync')
+        
     }
 }
 
