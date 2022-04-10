36a9818fad3e313f43d3bb1c239e4c25134ee018
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 17:19:19 -0500

    added new imports for webhook client and created variables for both webhooks that will be established

--- utils/sync.js
+++ utils/sync.js
@@ -1,12 +1,14 @@
 const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
-const {MessageEmbed, CommandInteractionOptionResolver, Message} = require('discord.js');
+const {MessageEmbed, CommandInteractionOptionResolver, Message, WebhookClient, WebhookClient} = require('discord.js');
 const { serverOne, serverTwo } = require('../config.json') 
 
 
 const syncMessage = (msg) => {
     const userPfp = msg.author.user.avatarURL()
     const userName = msg.author.user.username
-    
+
+    const webhookOne = new WebhookClient({ url: `` });
+    const webhookTwo = new WebhookClient({ url: ``});
 }
 
 export default syncMessage
