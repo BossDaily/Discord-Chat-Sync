5931c2c6c3fb86ce67c8d1c8fafb9acbee1020fe
BossDaily <comparatorcraft2020@gmail.com>
2022-04-10 17:25:17 -0500

    added a variable for the message content

--- utils/sync.js
+++ utils/sync.js
@@ -6,12 +6,13 @@ const { serverOne, serverTwo } = require('../config.json')
 const syncMessage = (msg) => {
     const userPfp = msg.author.user.avatarURL()
     const userName = msg.author.user.username
+    const userContent = msg.content
 
     const webhookOne = new WebhookClient({ url: `${serverOne.webhook}` });
     const webhookTwo = new WebhookClient({ url: `${serverTwo.webhook}`});
 
     if(msg.channelid === serverOne.id){
-
+        
     } else if (msg.channelid === serverTwo.id){
 
     } else {
