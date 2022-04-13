c592afa87594bb25a3d9ca70b5e478ebaddafc23
BossDaily <comparatorcraft2020@gmail.com>
2022-04-12 19:12:40 -0500

    comments

--- utils/sync.js
+++ utils/sync.js
@@ -4,14 +4,16 @@ const { serverOne, serverTwo } = require('../config.json')
 
 
 const syncMessage = (msg) => {
+    //Variables for djs components that will be reused 
     const userPfp = msg.member.displayAvatarURL({format: 'png'})
     const userName = msg.member.displayName
     const userContent = msg.content
     const channelid = msg.channelId.toString()
-
+    //variables for the two webhooks
     const webhookOne = new WebhookClient({ url: `${serverOne.webhook}` });
     const webhookTwo = new WebhookClient({ url: `${serverTwo.webhook}`});
 
+    //conditional logic for sending the webhooks
     if(channelid === serverOne.channel){
         webhookTwo.send({
             content: `${userContent}`,
